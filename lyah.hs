f8a7dd51e058565c07da8759d0555e4d3f68d2ad
ggorg03 <gustavobagorgonio03@gmail.com>
2018-03-03 19:18:41 -0300

    more fuctions :)

--- lyah.hs
+++ lyah.hs
@@ -37,6 +37,30 @@ g_drop 0 y = y
 g_drop x [] = []
 g_drop x (y:ys) = g_drop (x - 1) ys
 
+g_min :: Ord a => [a] -> a
+g_min [] = undefined
+g_min [x] = x
+g_min (x:(y:ys)) = if x > y then g_min (y:ys) else g_min (x:ys)
+
+g_max :: Ord a => [a] -> a
+g_max [] = undefined
+g_max [x] = x
+g_max (x:(y:ys)) = if x > y then g_max (x:ys) else g_max (y:ys)
+
+g_sum :: Num a => [a] -> a
+g_sum [] = undefined
+g_sum [x] = x
+g_sum (x:xs) = x + g_sum xs
+
+g_product :: Num a => [a] -> a
+g_product [] = undefined
+g_product [x] = x
+g_product (x:xs) = x + g_product xs
+
+g_elem :: Ord a => a -> [a] -> Bool
+g_elem x [] = False
+g_elem x (y:ys) = if x == y then True else g_elem x ys
+
 -- factorial is not a lyah's fuctions, i did cause i want
 factorial :: Integer -> Integer
 factorial 0 = 1
